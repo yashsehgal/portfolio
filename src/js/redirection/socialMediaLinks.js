8c2f2c289d4b459b48313c9b430a149b52afeeda
yashsehgal <yashsehgal.work@gmail.com>
2020-10-02 22:36:17 +0530

    filebase modified

---
+++ src/js/redirection/socialMediaLinks.js
@@ -0,0 +1,59 @@
+
+/// Declaring social media profile links
+const linkedInURL = "https://www.linkedin.com/in/yash-sehgal-55b7711a4/";
+const youtubeURL = "https://www.youtube.com/channel/UC23yA3SBkV_ehY4H8VSuNVg?";
+const instagramURL = "https://www.instagram.com/yash_sl24";
+const githubURL = "https://www.github.com/yashsehgal";
+const behanceURL = "https://www.behance.net/yashsehgaleee3";
+
+/// Discord Profile Details
+const discordUsername = "yashsehgal";
+const discordCode = "#4591";
+
+const getLinkedInURL = function getLinkedInURL() {
+  return linkedInURL;
+};
+
+const getYoutubeURL = function getYoutubeURL() {
+  return youtubeURL;
+};
+
+const getInstagramURL = function getInstagramURL() {
+  return instagramURL;
+};
+
+const getGithubURL = function getGithubURL() {
+  return githubURL;
+};
+
+const getBehanceURL = function getBehanceURL() {
+  return behanceURL;
+};
+
+const getDiscordUsername = function getDiscordUsername() {
+  return discordUsername;
+};
+
+const getDiscordCode = function getDiscordCode() {
+  return discordCode;
+};
+
+let redirectToLinkedInProfile = function redirectToLinkedInProfile() {
+  window.open(getLinkedInURL());
+};
+
+let redirectToYoutubeChannel = function redirectToYoutubeChannel() {
+  window.open(getYoutubeURL());
+};
+
+let redirectToInstagramProfile = function redirectToInstagramProfile() {
+  window.open(getInstagramURL());
+};
+
+let redirectToGitHubProfile = function redirectToGitHubProfile() {
+  window.open(getGithubURL());
+};
+
+let redirectToBehanceProfile = function redirectToBehanceProfile() {
+  window.open(getBehanceURL());
+};
