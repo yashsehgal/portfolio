eb5a67dd221f182c2098116f2c4ab951338b958c
yashsehgal <yashsehgal.work@gmail.com>
2020-10-02 19:55:59 +0530

    redirection/redirectToGitHubProfile.js updated

---
+++ src/js/redirection/redirectToGitHubProfile.js
@@ -0,0 +1,23 @@
+const githubYashSehgalURL = "https://www.yashsehgal.com/yashsehgal";
+const githubURL = "https://www.github.com/";
+const username = "yashsehgal";
+
+const redirectToYashSehgalGithub = function redirectToGithub() {
+  window.open(githubYashSehgalURL);
+};
+
+const redirectToGithub = function redirectToGithub() {
+  window.open(githubURL);
+};
+
+const getGithubUsername = function getGithubUsername() {
+  return username;
+};
+
+const getGithubURL = function getGithubURL() {
+  return githubURL;
+};
+
+const getYashSehgalGithubURL = function getYashSehgalGithubURL() {
+  return githubYashSehgalURL;
+}
