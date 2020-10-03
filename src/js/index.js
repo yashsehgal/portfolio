344e6572504e512640ec0d9c10486a8674e54399
yashsehgal <yashsehgal.work@gmail.com>
2020-10-04 00:14:31 +0530

    javascript backend update. Button Redirection methods are added to this file

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const schoolWebsite = "https://www.ghpsindore.org/";
+const collegeWebsite = "https://www.medicaps.ac.in/";
+
+const redirectToSchoolWebsite = function redirectToSchoolWebsite() {
+  window.open(schoolWebsite);
+};
+
+const redirectToCollegeWebsite = function redirectToCollegeWebsite() {
+  window.open(collegeWebsite);
+};
+
