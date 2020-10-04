b2c0e2fab6316ae2b26f5508392b4cf5c1c724fb
yashsehgal <yashsehgal.work@gmail.com>
2020-10-04 16:57:05 +0530

    javascript code update

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,17 @@
 const schoolWebsite = "https://www.ghpsindore.org/";
 const collegeWebsite = "https://www.medicaps.ac.in/";
 
+const contactMePagePath = "/src/contactme/contactme.htm";
+
 const redirectToSchoolWebsite = function redirectToSchoolWebsite() {
   window.open(schoolWebsite);
 };
 
 const redirectToCollegeWebsite = function redirectToCollegeWebsite() {
   window.open(collegeWebsite);
+};
+
+const redirectToContactMePage = function redirectToContactMePage() {
+  window.open(contactMePagePath);
 };
 
