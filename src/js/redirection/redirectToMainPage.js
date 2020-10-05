d2b90e7a898a650af24d09a31b6b95c2925d2696
yashsehgal <yashsehgal.work@gmail.com>
2020-10-05 21:24:05 +0530

    javascript code has been modified

--- src/js/redirection/redirectToMainPage.js
+++ src/js/redirection/redirectToMainPage.js
@@ -1,4 +1,10 @@
 // const homePagePath = PATH AFTER UPLOADING THIS SITE ONLINE;
-const redirectToMainPage = function redirectToMainPage() {
-  console.log("main");
-}
+const homePageName = "index.htm";
+const homePageRelativePath = "/src/pages/index.htm";
+
+let redirectToMainPage = function redirectToMainPage() {
+  // console.log("main");
+  console.log(
+    "[filename: " + homePageName + ", filepath: " + homePageRelativePath + "]. This file is currently not available in the file system"
+  );
+};
