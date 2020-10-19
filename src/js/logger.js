fa1d4194de8095ffbb52b69d74d098e04caa7938
yashsehgal <yashsehgal.work@gmail.com>
2020-10-19 23:09:02 +0530

    javascript logger module is added to the project

---
+++ src/js/logger.js
@@ -0,0 +1,39 @@
+
+/// setting null operation using an instance
+const __nothing__ = '\0';
+
+class Logger {
+  messageTitle = __nothing__;
+  messageTextCommand = __nothing__;
+  portAddress = __nothing__;
+  portName = __nothing__;
+  messageType = __nothing__;
+  portAccess = false;       /// by-default there will be no access
+
+  constructor(portAccess = false) {
+    portAccess = true;
+  };
+
+  setMessageTitle(messageTitle) {
+    if (this.portAccess) {
+      if (this.messageType === "log") {
+        console.log("> " + messageTitle);
+      } else if (this.messageType === "warn") {
+        console.warn("warning: " + messageTitle);
+      } else if (this.messageType === "error") {
+        console.error("error: " + messageTitle);
+      } else if (this.messageType === "info") {
+        console.log("info: " + messageTitle)
+      } else {
+        console.warn("warning: message type is not specified before being used");
+        console.info("deploying same message title with log method");
+        console.log("> " + messageTitle);
+      }
+      /// saving this message into cache logger file
+      this.messageTitle = messageTitle;
+    }
+  }
+  getMessageTitle() {
+    return this.messageTitle;
+  };
+};
