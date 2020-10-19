004593c1698b97b3304c8de06d754e16660447c1
yashsehgal <yashsehgal.work@gmail.com>
2020-10-20 01:54:58 +0530

    certification link redirection method is added to the project codebase

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 const schoolWebsite = "https://www.ghpsindore.org/";
 const collegeWebsite = "https://www.medicaps.ac.in/";
+const pythonBasicCertificateLink = "https://www.hackerrank.com/certificates/5d2540a9e309";
 
 /// this path string will be changed with the actual web URL after this website will be uploaded.
 /// contactMePagePath, stores the relative path of contactme.htm page (HTML File)...
@@ -17,4 +18,8 @@ const redirectToCollegeWebsite = function redirectToCollegeWebsite() {
 const redirectToContactMePage = function redirectToContactMePage() {
   window.open(contactMePagePath);
 };
+
+const redirectToPythonBasicCertification = function redirectToPythonBasicCertification() {
+  window.open(pythonBasicCertificateLink)
+}
 
